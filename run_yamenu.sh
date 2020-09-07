14c83d8fa748e1517119aeb65acc429827492cc5
Lukas Krickl <lukas@krickl.dev>
2020-09-07 19:24:23 +0200

    Updated run script to only include GUI programs

--- run_yamenu.sh
+++ run_yamenu.sh
@@ -3,5 +3,6 @@
 # this script executes yamenu with all possible commands
 # this script depends on bash!
 
-programs=$(compgen -c | sort | tr '\n' ';')
+# programs=$(compgen -c | sort | tr '\n' ';')
+programs=$(ls /usr/share/applications | awk -F '.desktop' '{ print $1 }' | sort | tr '\n' ';')
 yamenu -p"$programs"
